d2485dd7af4011303a969ff8e75b50e347d33320
ivoronline <ivoronline@gmail.com>
2024-04-28 20:48:22 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_database_jdbctemplate_select/controller/MyController.java
@@ -0,0 +1,56 @@
+package com.ivoronline.springboot_database_jdbctemplate_select.controller;
+
+import com.ivoronline.springboot_database_jdbctemplate_select.dto.PersonDTO;
+import com.ivoronline.springboot_database_jdbctemplate_select.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
+
+@RestController
+public class MyController {
+
+  //PROPERTIES
+  @Autowired private MyService myService;
+
+  //=========================================================================================================
+  // SELECT RECORD
+  //=========================================================================================================
+  // Request             => http://localhost:8080/selectRecord?id=1&age=10
+  // Returns JSON Object => { "id":1, "name":"John", "age":10 }
+  @ResponseBody
+  @GetMapping("/selectRecord")
+  public PersonDTO selectRecord(@RequestParam Integer id, @RequestParam Integer age) {
+    PersonDTO personDTO = myService.selectRecord(id, age);
+    return personDTO;
+  }
+
+  //=========================================================================================================
+  // SELECT RECORDS
+  //=========================================================================================================
+  // Request            => http://localhost:8080/selectRecords?id=1&age=10
+  // Returns JSON Array => [ {"id":2, "name":"Bill", "age":20 }, { "id":3, "name":"Susan", "age":30 } ]
+  @ResponseBody
+  @GetMapping("/selectRecords")
+  public List<PersonDTO> selectRecords(@RequestParam Integer id, @RequestParam Integer age) {
+    List<PersonDTO> personDTOList = myService.selectRecords(id, age);
+    return personDTOList;
+  }
+
+  //=========================================================================================================
+  // SELECT WITHOUT PARAMETERS
+  //=========================================================================================================
+  // Request without Parameters => http://localhost:8080/selectWithoutParameters
+  // Returns JSON Array         => [ {"id":2, "name":"Bill", "age":20 }, { "id":3, "name":"Susan", "age":30 } ]
+  @ResponseBody
+  @GetMapping("/selectWithoutParameters")
+  public List<PersonDTO> selectWithoutParameters() {
+    List<PersonDTO> personDTOList = myService.selectWithoutParameters();
+    return personDTOList;
+  }
+
+}
---
+++ src/main/java/com/ivoronline/springboot_database_jdbctemplate_select/service/MyService.java
@@ -0,0 +1,49 @@
+package com.ivoronline.springboot_database_jdbctemplate_select.service;
+
+import com.ivoronline.springboot_database_jdbctemplate_select.dto.PersonDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class MyService {
+
+  //PROPERTIES
+  @Autowired private JdbcTemplate jdbcTemplate;
+
+  //=========================================================================================================
+  // SELECT RECORD
+  //=========================================================================================================
+  public PersonDTO selectRecord(Integer id, Integer age) {
+    return (PersonDTO) jdbcTemplate.queryForObject(
+      " SELECT * FROM PERSON WHERE ID = ? AND AGE = ?"   //Return single Record
+      , new Object[] { id, age }
+      , new BeanPropertyRowMapper(PersonDTO.class)
+    );
+  }
+
+  //=========================================================================================================
+  // SELECT RECORDS
+  //=========================================================================================================
+  public List<PersonDTO> selectRecords(Integer id, Integer age) {
+    return jdbcTemplate.query(
+      " SELECT * FROM PERSON WHERE ID > ? AND AGE > ?"   //Return multiple Records
+      , new Object[] { id, age }
+      , new BeanPropertyRowMapper(PersonDTO.class)
+    );
+  }
+
+  //=========================================================================================================
+  // SELECT WITHOUT PARAMETERS
+  //=========================================================================================================
+  public List<PersonDTO> selectWithoutParameters() {
+    return jdbcTemplate.query(
+      " SELECT * FROM PERSON WHERE ID > 1 AND AGE > 10"  //Without SQL Parameters
+    //, new Object[] { }                                 //new Object[] { } or skip this Function Parameter
+      , new BeanPropertyRowMapper(PersonDTO.class)
+    );
+  }
+
+}
